2b476bc05569ad90ff52780d964589f6e19e08f4
deejaseethal <deejaseethal@gmail.com>
2025-07-03 13:32:47 +0400

    deleted files

--- src/app/signin/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function SigninPage() {
-    return (
-      <div className="min-h-screen flex items-center justify-center">
-        <h1 className="text-3xl font-bold">Signin Page</h1>
-      </div>
-    );
-  }
